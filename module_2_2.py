c691008cd414aade3742bc07217c7a82e3e21772
exodise0404 <exodise0404@gmail.com>
2024-07-26 18:41:36 +0800

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -1,9 +1,9 @@
 first = int(input("Введите число №1: "))
 second = int(input("Введите число №2: "))
 third = int(input("Введите число №3: "))
-if first == second and first == third and first == third:
+if first == second == third:
     print(3)
-elif first == second or first == third or first == third:
+elif first == second or first == third or second == third:
     print(2)
-elif first != second and first != third and first != third:
+else:
     print(0)
